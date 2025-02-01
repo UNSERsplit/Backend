9915b74fde78805e1942741ba5d9cde205a5d121
Noah <82201000+Nopro263@users.noreply.github.com>
2025-02-01 20:46:34 +0100

    added alembic

---
+++ src/database.py
@@ -0,0 +1,16 @@
+import os
+from sqlmodel import create_engine, Session
+
+from typing import Annotated
+from fastapi import Depends
+
+engine = create_engine(os.environ["DB_URL"])
+
+def _get_db():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
+
+DB = Annotated[Session, Depends(_get_db)]
