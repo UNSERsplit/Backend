738541bdf1a3dd37279c2a4ab6f76ed7c9d3344f
johabba <j_ha@gmx.at>
2025-02-07 08:41:27 +0100

    added user

---
+++ src/models/User.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+from typing import Optional
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: EmailStr = Field(unique=True)
+    name: str
+    password: str
+    iban: str
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
+from . import User
+
 __all__ = [
-    
+    "User"
 ]
