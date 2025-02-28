58bbaab7efb903e6cc978bfee333a8e41b6300ef
Noah <82201000+Nopro263@users.noreply.github.com>
2025-02-28 10:02:14 +0100

    d

--- src/routes/User.py
+++ src/routes/User.py
@@ -17,7 +17,7 @@ def getAllUsers(db: DB) -> List[User]:
 """get public data from User"""
 @userrouter.get("/{userid}")
 def getUserById(db: DB, userid: int) -> PublicUserData:
-    return db.exec(select(User).where(User.userid == userid))
+    return db.exec(select(User).where(User.userid == userid)).one()
 
 
 """register user"""
