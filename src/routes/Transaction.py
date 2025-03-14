3c3504d03f5a63d435388ed1e63ec3efff8da5d4
timk20071 <tim.kainhofer@gmx.at>
2025-03-14 09:09:17 +0100

    Added Transaction logics

--- src/routes/Transaction.py
+++ src/routes/Transaction.py
@@ -1,25 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends
 from ..database import DB
 from typing import List
 from sqlmodel import select
+from sqlalchemy import or_, and_
 from ..models.Transaction import Transaction, TransactionCreateRequest
+from ..auth import get_loggedin_user, get_current_user
+from ..models.User import User
 
 transactionRouter = APIRouter(prefix="/api/transactions")
 
 """get all my past transactions"""
 @transactionRouter.get("/me")
-def getAllTransactionsOfUser() -> List[Transaction]:
-    return 0
+def getAllTransactionsOfUser(db: DB, current_user: User = Depends(get_current_user)) -> List[Transaction]:
+    return db.exec(select(Transaction).where(or_(Transaction.fromuserid == current_user.userid, Transaction.touserid == current_user.userid))).all()
 
 
 """get all transactions between me and other"""
 @transactionRouter.get("/to/{touserid}")
-def getTransactionsBetweenUsers(touserid: int) -> List[Transaction]:
-    return 0
+def getTransactionsBetweenUsers(touserid: int, db: DB, current_user: User = Depends(get_current_user)) -> List[Transaction]:
+    return db.exec(select(Transaction).where(or_(and_(Transaction.fromuserid == current_user.userid, Transaction.touserid == touserid), and_(Transaction.fromuserid == touserid, Transaction.touserid == current_user.userid)))).all()
+
 
 """create new transaction"""
 @transactionRouter.post("/")
 def addTransaction(db: DB, transaction: TransactionCreateRequest) -> Transaction:
-    return 0
+    transaction = Transaction(**transaction.model_dump())
+    db.add(transaction)
+    db.commit()
+    db.refresh(transaction)
+    return transaction
 
 
