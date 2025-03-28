77e8c8cc8f7f35f7260af9de4755c206be23c549
jonhabba <tim.kainhofer@gmx.at>
2025-03-28 10:29:23 +0100

    Added Access restrictios in Group-Api

--- src/routes/Group.py
+++ src/routes/Group.py
@@ -3,13 +3,15 @@ from sqlalchemy.sql.functions import current_user
 
 from ..database import DB
 from typing import List
-from sqlmodel import select, and_
+from sqlmodel import select
+from sqlalchemy import or_, and_
 from ..models.Group import Group, GroupCreationRequest
 from ..models.User import User, PublicUserData
 from ..models.GroupMembers import GroupMembers
 from ..auth import get_current_user
 
 grouprouter = APIRouter(prefix="/api/group")
+
 
 @grouprouter.get("/")
 def getAllGroupsOfUser(db: DB, current_user: User = Depends(get_current_user)) -> List[Group]:
@@ -54,8 +56,8 @@ def updateGroup(groupid: int, group: GroupCreationRequest, db: DB) -> Group:
 
 
 @grouprouter.post("/{groupid}/users")
-def addUserToGroup(groupid: int, userid: int, db: DB, current_user: User = Depends(get_current_user)) -> GroupMembers:  # accepted invite and admin accepts user
-    """add user to group [ADMIN]"""
+def addUserToGroup(groupid: int, userid: int, db: DB, current_user: User = Depends(get_current_user)) -> GroupMembers:
+    """add user to group"""
 
     groupmember = db.exec(select(GroupMembers).where(and_(groupid == GroupMembers.groupid,userid == GroupMembers.userid))).one()
     groupmember.pending = False
@@ -66,7 +68,7 @@ def addUserToGroup(groupid: int, userid: int, db: DB, current_user: User = Depen
 
 @grouprouter.post("/{groupid}/users/{userid}/invite")
 def inviteUserToGroup(db: DB, groupid: int, userid: int, current_user: User = Depends(get_current_user)) -> GroupMembers:  # send invite
-    """invite user to group"""
+    """invite user to group [ADMIN]"""
     group = db.exec(select(Group).where(Group.groupid == groupid)).one()
     if current_user.userid != group.adminuser_userid:
         raise HTTPException(status_code=403, detail="You are not allowed to invite to this group")
@@ -85,12 +87,15 @@ def deleteUserFromGroup(groupid: int, userid: int, db: DB, current_user: User = 
         raise HTTPException(status_code=403, detail="You are not allowed to invite to this group")
     memberofgroup = db.exec(select(GroupMembers).where(and_(GroupMembers.groupid == groupid, GroupMembers.userid == userid))).one()
 
-
     db.delete(memberofgroup)
     db.commit()
     return memberofgroup
 
 @grouprouter.get("/{groupid}/users")
 def getUsersOfGroup(db: DB, groupid: int, current_user: User = Depends(get_current_user)) -> List[PublicUserData]:
-    """get all users in group"""
+    """get all users in group [only Groupmembers]"""
+    getusers = db.exec(select(GroupMembers).where(and_(GroupMembers.groupid == groupid, GroupMembers.userid == current_user.userid))).all()
+    print(getusers)
+    if getusers == []:
+        raise HTTPException(status_code=403, detail="Not allowed to access this data")
     return db.exec(select(User).join(GroupMembers, User.userid == GroupMembers.userid).where(and_(GroupMembers.groupid == groupid, GroupMembers.pending == False))).all()
