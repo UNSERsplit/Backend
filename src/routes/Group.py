6a791d6b544814e5286b492d77ce1a9bfded5391
Noah <82201000+Nopro263@users.noreply.github.com>
2025-05-13 10:41:01 +0200

    fixed search

--- src/routes/Group.py
+++ src/routes/Group.py
@@ -21,7 +21,7 @@ def getAllGroupsOfUser(db: DB, current_user: User = Depends(get_current_user)) -
     return db.exec(select(Group).join(GroupMembers, Group.groupid == GroupMembers.groupid).where(current_user.userid == GroupMembers.userid)).all()
 
 
-@grouprouter.get("/{groupid}")
+@grouprouter.get("/{groupid:int}")
 def getGroupByID(groupid: int, db: DB) -> Group:
     """get group"""
 
