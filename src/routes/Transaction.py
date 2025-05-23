1be100f3a6e16d3c6900530b7238806773f2b048
timk20071 <tim.kainhofer@gmx.at>
2025-05-23 09:09:07 +0200

    Added restriction to transaction so it is only possible to post them between fiends

--- src/routes/Transaction.py
+++ src/routes/Transaction.py
@@ -6,6 +6,7 @@ from sqlalchemy import or_, and_
 from ..models.Transaction import Transaction, TransactionCreateRequest
 from ..auth import get_current_user
 from ..models.User import User
+from ..models.Friends import Friends
 
 from datetime import date
 
@@ -27,12 +28,14 @@ def getTransactionsBetweenUsers(touserid: int, db: DB, current_user: User = Depe
 
 @transactionRouter.post("/")
 def addTransaction(db: DB, transaction: TransactionCreateRequest, current_user: User = Depends(get_current_user)) -> Transaction:
-    """create new transaction"""
+    """create new transaction (only between friends)"""
 
     transaction = Transaction(**transaction.model_dump(), fromuserid=current_user.userid, date=date.today())
+
+    if db.exec(select(Friends).where(and_(or_(and_(transaction.touserid == Friends.invited_userid, transaction.fromuserid == Friends.inviting_userid), and_(transaction.fromuserid == Friends.invited_userid, transaction.touserid == Friends.inviting_userid)), Friends.pending == False))).one_or_none() is None:
+        raise HTTPException(status_code=405, detail="Not allowed to invite this user, only friends")
+
     db.add(transaction)
     db.commit()
     db.refresh(transaction)
     return transaction
-
-
